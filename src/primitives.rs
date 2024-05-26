6bdd1ef5b13f533905692d70ff7cd4cd8ca6cc51
Masa Maeda <masamaedae@gmail.com>
2024-05-26 12:42:50 +0900

    Changes the buffers element from a map to a vec because after changing to using the usize id directly

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::atomic::AtomicUsize};
+use std::sync::atomic::AtomicUsize;
 
 use crate::{RenderParams, Vertex};
 use bevy_ecs::component::Component;
@@ -6,6 +6,8 @@ use glium::{
     dynamic_uniform, glutin::surface::WindowSurface, Display, DrawParameters, Program, Surface,
     VertexBuffer,
 };
+
+static DATA_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 #[derive(Clone, Copy)]
 pub enum Type {
@@ -26,7 +28,9 @@ impl From<Type> for glium::index::PrimitiveType {
 
 pub struct Renderer {
     program: Program,
-    buffers: HashMap<usize, VertexBuffer<Vertex>>,
+    // Maybe it could be a good idea to just have the array be a fixed size if this becomes a performance
+    // Issue and set it with a constant if we would like to reduce indirection
+    buffers: Vec<Option<VertexBuffer<Vertex>>>,
 }
 
 impl Renderer {
@@ -63,7 +67,8 @@ impl Renderer {
         let program = glium::Program::new(display, source).unwrap();
 
         Self {
-            buffers: Default::default(),
+            // Have a sufficiently large base capacity so we don't have to resize the array as much
+            buffers: Vec::with_capacity(64),
             program,
         }
     }
@@ -74,14 +79,25 @@ impl Renderer {
         };
 
         let buffer = {
-            if data.dirty || !self.buffers.contains_key(&data.id) {
+            if data.dirty || self.buffers.len() <= data.id || self.buffers[data.id].is_none()  {
                 let buffer =
                     glium::VertexBuffer::new(render_params.display, &data.primitive_data).unwrap();
-                self.buffers.insert(data.id, buffer);
+                // If the vector is not large enough to contain the new buffer then we resize based on
+                // the largeset id we have available
+                // Note: with this solution if we create and delete primitive data objects enough
+                // we will be running into a sort of memory leak where the id keeps going up but old
+                // id's are never reused so going forward this may not be the best solution
+                if self.buffers.len() <= data.id {
+                    let largest = DATA_ID_COUNTER.load(std::sync::atomic::Ordering::Relaxed);
+                    self.buffers.resize_with(largest, || None);
+                }
+
+                self.buffers[data.id] = Some(buffer);
                 data.dirty = false;
             }
 
-            &self.buffers[&data.id]
+            // Safety: Based on above code we can guarentee that we have a buffer that has been created
+            self.buffers[data.id].as_ref().unwrap()
         };
 
         render_params
@@ -108,8 +124,7 @@ pub struct Data<'a> {
 
 impl Data<'_> {
     pub fn new(points: &[Vertex], primitive_type: Type, size: f32) -> Self {
-        static ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
-        let id = ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        let id = DATA_ID_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
 
         let primitive_data = points.to_vec();
 
