558aa5d1fbcc6b4e57efd5bcd822723e4de8fe40
Masa Maeda <masamaedae@gmail.com>
2024-05-26 20:58:10 +0900

    has create_control_point return EntityCommands instead of just an entity so we can do mutations to individual points

--- src/control_points.rs
+++ src/control_points.rs
@@ -1,7 +1,7 @@
 use bevy_ecs::{
     component::Component,
     entity::Entity,
-    system::{Commands, Resource},
+    system::{Commands, EntityCommands, Resource},
 };
 
 use crate::{
@@ -26,23 +26,21 @@ pub struct Point {
     pub size: f32,
 }
 
-fn create_control_point(mut commands: Commands, x: f32, y: f32) -> Entity {
-    commands
-        .spawn((
-            Position::new(x, y),
-            Point { size: 15.0 },
-            Hoverable { radius: 20.0 },
-            Draggable,
-        ))
-        .id()
+fn create_control_point<'commands>(commands: &'commands mut Commands, x: f32, y: f32) -> EntityCommands<'commands> {
+    commands.spawn((
+        Position::new(x, y),
+        Point { size: 15.0 },
+        Hoverable { radius: 20.0 },
+        Draggable,
+    ))
 }
 
 pub fn initialize_points(mut commands: Commands) {
-    let start_handle = create_control_point(commands.reborrow(), 400.0, 456.0);
-    let end_handle = create_control_point(commands.reborrow(), 400.0, 24.0);
+    let start_handle = create_control_point(&mut commands, 400.0, 456.0).id();
+    let end_handle = create_control_point(&mut commands, 400.0, 24.0).id();
 
-    let start_point = create_control_point(commands.reborrow(), 200.0, 240.0);
-    let end_point = create_control_point(commands.reborrow(), 600.0, 240.0);
+    let start_point = create_control_point(&mut commands, 200.0, 240.0).id();
+    let end_point = create_control_point(&mut commands, 600.0, 240.0).id();
 
     commands.insert_resource(ControlPoints {
         start_point,
